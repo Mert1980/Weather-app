1168db6c7943e4c3f97c3476f018750e972feeff
Mert Demirok <mertdemirok80@gmail.com>
2019-12-24 00:07:22 +0100

    language of the summary is changed to Turkish

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 const url =
-  "https://api.darksky.net/forecast/04b7a140f48a161bab606bc04471c4fe/37.8267,-122.4233?units=si&";
+  "https://api.darksky.net/forecast/04b7a140f48a161bab606bc04471c4fe/37.8267,-122.4233?units=si&lang=tr";
 
 request({ url: url, json: true }, (error, response) => {
   // const data = JSON.parse(response.body)
