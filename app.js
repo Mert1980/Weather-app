ccbd35c334b3924151f670957a2e507acb57a15b
Mert Demirok <mertdemirok80@gmail.com>
2019-12-23 23:48:42 +0100

    showing the current temperature and propability of rain via response data

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const request = require('request');
+const request = require("request");
 
-const url ='https://api.darksky.net/forecast/04b7a140f48a161bab606bc04471c4fe/37.8267,-122.4233'
+const url =
+  "https://api.darksky.net/forecast/04b7a140f48a161bab606bc04471c4fe/37.8267,-122.4233";
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
-})
+request({ url: url, json: true }, (error, response) => {
+  // const data = JSON.parse(response.body)
+  // console.log(data.currently)
+  // json:true ==> automatically parses JSON string
+  // console.log(response.body.currently);
+  const temp = response.body.currently.temperature;
+  const rain = response.body.currently.precipProbability;
+  console.log(
+    `It's currently ${temp} degrees out. There is a ${rain}% chance of rain.`
+  );
+});
