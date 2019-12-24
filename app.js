580b0fb9d86ad36b6d81c8033d78d2ae7e096598
Mert Demirok <mertdemirok80@gmail.com>
2019-12-24 13:25:08 +0100

    fecthing latitude and longitude from Mapbox using location name

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 const url =
-  "https://api.darksky.net/forecast/04b7a140f48a161bab606bc04471c4fe/37.8267,-122.4233?units=si&lang=tr";
+  "https://api.darksky.net/forecast/04b7a140f48a161bab606bc04471c4fe/37.8267,-122.4233?units=si";
 
 request({ url: url, json: true }, (error, response) => {
   // const data = JSON.parse(response.body)
@@ -15,3 +15,18 @@ request({ url: url, json: true }, (error, response) => {
     `${dailySummary} It's currently ${temp} degrees out. There is a ${rain}% chance of rain.`
   );
 });
+
+// Geocoding
+// Address --> Lat/Long --> Weather
+
+const urlMapbox =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWF0dDE5ODAiLCJhIjoiY2s0anNld2xmMHpoZDNsbnRoaGk0M250dSJ9.5WtKXyPXAsexEUjjFgeqQA&limit=1";
+
+request ({url: urlMapbox, json: true}, (error, response) => {
+  const latitude = response.body.features[0].center[1];
+  const longitude = response.body.features[0].center[0];
+  console.log(`Lat: ${latitude}, Long:${longitude}`);
+});
+
+
+
