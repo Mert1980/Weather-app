d30978ba8c395f7c213bc24a367d89950315f81c
Mert Demirok <mertdemirok80@gmail.com>
2019-12-25 15:50:12 +0100

    destructured forecast function(response.body)

--- utils/forecast.js
+++ utils/forecast.js
@@ -6,15 +6,15 @@ const url =
 const forecast = (longitude, latitude, callback) => {
   const url = `https://api.darksky.net/forecast/04b7a140f48a161bab606bc04471c4fe/${longitude},${latitude}?units=si`;
 
-  request({ url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, {body}) => {
     if (error) {
       callback("Unable to connect to weather service!", undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback("Poorly formatted request", undefined);
     } else {
-      const temp = response.body.currently.temperature;
-      const rain = response.body.currently.precipProbability;
-      const dailySummary = response.body.daily.data[0].summary;
+      const temp = body.currently.temperature;
+      const rain = body.currently.precipProbability;
+      const dailySummary = body.daily.data[0].summary;
       callback(
         undefined,
         `${dailySummary} It's currently ${chalk.inverse.yellow(`${temp}`)} Celcius degrees out. There is a ${chalk.inverse.yellow(`${rain}%`)} chance of rain.`
