82714396dfe0c84ee778d75ef71564610b7e67f9
Mert Demirok <mertdemirok80@gmail.com>
2019-12-25 13:08:45 +0100

    callback chaining completed. Added command line argument with process.argv

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const geoCode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
-const chalk = require('chalk');
+const chalk = require("chalk");
 
-geoCode("balikesir", (error, geoData) => {
-  if (error){
-    return console.log(error)
-  }
-  forecast(geoData.latitude, geoData.longitude, (error, forecastData) => {
-    if (error){
-      return console.log(error)
+const address = process.argv[2];
+if (!address) {
+  console.log("Please provide an address!");
+} else {
+  geoCode(command, (error, geoData) => {
+    if (error) {
+      return console.log(error);
     }
-    console.log(chalk.inverse(geoData.location));
-    console.log(forecastData);
+    forecast(geoData.latitude, geoData.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(chalk.inverse(geoData.location));
+      console.log(forecastData);
+    });
   });
-});
-
-
+}
