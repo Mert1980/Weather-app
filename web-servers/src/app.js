d6cbb4ec08c4d99a561869908760aadddfb01696
Mert Demirok <mertdemirok80@gmail.com>
2019-12-26 20:54:36 +0100

    serving up HTML and JSON

--- web-servers/src/app.js
+++ web-servers/src/app.js
@@ -3,19 +3,22 @@ const express = require("express"); // express is a function
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Express!");
+  res.send("<h1>Welcome</h1>");
 });
 
 app.get("/help", (req, res) => {
-  res.send("Help Page");
+  res.send([{ name: "Mert" }, { name: "Almira" }]);
 });
 
 app.get("/about", (req, res) => {
-  res.send("It is all about weather!");
+  res.send("<h1>It is all about weather!</h1>");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Please find the current weather report");
+  res.send({
+    location: "Leuven",
+    forecast: "It is 5 degrees and 5% chance of rain"
+  });
 });
 
 app.listen(3000, () => {
