47bc63abd195d08ce92d416a4e657c8fa5307cf8
Mert Demirok <mertdemirok80@gmail.com>
2019-12-26 21:46:00 +0100

    serving up help and about static files with express

--- web-servers/src/app.js
+++ web-servers/src/app.js
@@ -3,24 +3,10 @@ const express = require("express"); // express is a function
 
 const app = express();
 
-// console.log(__dirname)
-// console.log(path.join(__dirname, '../public'))
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 // we pass the return value of express.static function into app.use
-
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome</h1>");
-});
-
-app.get("/help", (req, res) => {
-  res.send([{ name: "Mert" }, { name: "Almira" }]);
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>It is all about weather!</h1>");
-});
 
 app.get("/weather", (req, res) => {
   res.send({
