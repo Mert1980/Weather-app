02fa8b479830b551efcc0190f5446c61a2d7e721
Mert Demirok <mertdemirok80@gmail.com>
2019-12-27 11:34:16 +0100

    pointing express from views directory to the templates directory

--- web-servers/src/app.js
+++ web-servers/src/app.js
@@ -3,8 +3,10 @@ const express = require("express"); // express is a function
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates');
 
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 app.use(express.static(publicDirectoryPath))
 // we pass the return value of express.static function into app.use
 
