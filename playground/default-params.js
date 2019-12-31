13c2f98ae99f453866f2445e12bc76e554158dc0
Mert Demirok <mertdemirok80@gmail.com>
2019-12-31 07:13:47 +0100

    example added on leaving the first arg empty in a 2 arg function

--- playground/default-params.js
+++ playground/default-params.js
@@ -1,14 +1,14 @@
 // changing the default parameter from undefined to user
-const greeter = (name="user") => { 
-    console.log('Hello ' + name)
-}
+const greeter = (name = "user") => {
+  console.log("Hello " + name);
+};
 
-greeter("Mert")
+greeter("Mert");
 greeter();
 
-const transaction = (type, {label, stock=0} = {}) => {
-    console.log(type, label, stock)
-}
+const transaction = (type, { label, stock = 0 } = {}) => {
+  console.log(type, label, stock);
+};
 /* if we don't assign an empty object {} as a default value, the app crashes
 when we do not pass any argument other that "order" 
 
@@ -17,3 +17,9 @@ a default value
 
 */
 transaction("order");
+
+const greeting = (name = "New User", age = 100) => {
+  console.log(`Hello ${name}, you are ${age} years old.`);
+};
+
+greeting(25); // returns : Hello 25, you are 100 years old
